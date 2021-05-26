0581e514e40bcd1edab82b68ab27382ce00ddd4d
Luis Cajigas Martínez <luic87@gmail.com>
2021-05-26 20:42:36 +0200

    First commit

---
+++ live.py
@@ -0,0 +1,50 @@
+# This script will detect faces via your webcam.
+# Tested with OpenCV3
+
+import cv2
+
+cap = cv2.VideoCapture(0)
+
+# Create the haar cascade
+faceCascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
+eye_cascade = cv2.CascadeClassifier('haarcascades/haarcascade_eye.xml')
+
+while(True):
+	# Capture frame-by-frame
+	ret, frame = cap.read()
+
+	# Our operations on the frame come here
+	gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+	# Detect faces in the image
+	faces = faceCascade.detectMultiScale(
+		gray,
+		scaleFactor=1.1,
+		minNeighbors=5,
+		minSize=(30, 30)
+		#flags = cv2.CV_HAAR_SCALE_IMAGE
+	)
+
+	for (x,y,w,h) in faces:
+		cv2.rectangle(frame,(x,y),(x+w,y+h),(255,255,20),2)
+		roi_gray = gray[y:y+h, x:x+w]
+		roi_color = frame[y:y+h, x:x+w]
+		eyes = eye_cascade.detectMultiScale(roi_gray)
+		for (ex,ey,ew,eh) in eyes:
+			cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
+
+	# print("Found {0} faces!".format(len(faces)))
+
+	# Draw a rectangle around the faces
+	for (x, y, w, h) in faces:
+		cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+
+
+	# Display the resulting frame
+	cv2.imshow('frame', frame)
+	if cv2.waitKey(1) & 0xFF == ord('q'):
+		break
+
+# When everything done, release the capture
+cap.release()
+cv2.destroyAllWindows()
